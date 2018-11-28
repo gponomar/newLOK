ed0fc2f6c72bc38e1c410cc3f8eafa2b897b63eb
gponomar <gponomar@calpoly.edu>
2018-11-28 14:04:30 -0800

    added high score file input

--- src/HighScore.java
+++ src/HighScore.java
@@ -3,11 +3,17 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -24,11 +30,15 @@ public class HighScore {
 	public JButton homeBtnHighScore = new JButton(back);
 	ImageIcon HighScoreIcon = new ImageIcon("resource/HighScore.png");
 	JLabel labelHighScore = new JLabel(HighScoreIcon);
-	JLabel score1 = new JLabel("1.	" + "Score1");
-	JLabel score2 = new JLabel("2.	" + "Score2");
-	JLabel score3 = new JLabel("3.	" + "Score3");
-	JLabel score4 = new JLabel("4.	" + "Score4");
-	JLabel score5 = new JLabel("5.	" + "Score5");
+	//get list of top 5 scores from "resource/HighScoreList"
+	ArrayList<ArrayList<String>> highScoreList = 
+			new ArrayList<ArrayList<String>>();
+	
+	JLabel score1 = new JLabel("1.	");
+	JLabel score2 = new JLabel("2.	");
+	JLabel score3 = new JLabel("3.	");
+	JLabel score4 = new JLabel("4.	");
+	JLabel score5 = new JLabel("5.	");
 	public JPanel pnlHolderHighScore = new JPanel()
 	{
 		public void paintComponent(java.awt.Graphics g)
@@ -47,6 +57,23 @@ public class HighScore {
 			}
 		};
 	public HighScore() {
+		highScoreList = getTopScores();
+		
+		score1.setText("1.   " +
+				highScoreList.get(0).get(0) +
+				"   " + highScoreList.get(0).get(1));
+		score2.setText("2.   " +
+				highScoreList.get(1).get(0)+ 
+				"   " + highScoreList.get(1).get(1));
+		score3.setText("3.   " +
+				highScoreList.get(2).get(0)+ 
+				"   " + highScoreList.get(2).get(1));
+		score4.setText("4.   " +
+				highScoreList.get(3).get(0)+ 
+				"   " + highScoreList.get(3).get(1));
+		score5.setText("5.   " +
+				highScoreList.get(4).get(0)+ 
+				"   " + highScoreList.get(4).get(1));
 		GridBagConstraints gbcBack = new GridBagConstraints();
 		gbcBack.anchor = GridBagConstraints.WEST;
 		GridBagConstraints gbcTitle = new GridBagConstraints();
@@ -102,5 +129,55 @@ public class HighScore {
             }
         });
     }
+	public ArrayList<ArrayList<String>> getTopScores() {
+		ArrayList<ArrayList<String>> answer = new ArrayList<ArrayList<String>>();
+		ArrayList<String> inner = new ArrayList<String>();
+		try
+		{
+			Scanner scanner = new Scanner(new File("resource/HighScoreList"));
+			while (scanner.hasNext())
+			{
+				String line = scanner.next();
+				inner.add(line);
+				//System.out.println(line);
+				
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		ArrayList<String> temp = new  ArrayList<String>();
+		for(int i = 0; i<= inner.size()-1; i+=2)
+		{
+			temp = new ArrayList<String>();
+			temp.add(inner.get(i));
+			temp.add(inner.get(i+1));
+			answer.add(temp);
+		}
+		for(int j = 0; j< answer.size(); j++)
+		{
+			System.out.printf("Before %d: %s %s\n", j, answer.get(j).get(0), answer.get(j).get(1));
+		}
+		
+		Collections.sort(answer, new Comparator<ArrayList<String>>() {
+			@Override
+			public int compare(ArrayList<String> a, ArrayList<String>b) {
+				if (Integer.parseInt(a.get(0)) > Integer.parseInt((b.get(0))))
+				{
+					return -1;
+				}
+				else if (Integer.parseInt(a.get(0)) < Integer.parseInt((b.get(0))))
+				{
+					return 1;
+				}
+				else return 0;
+			}
+		});
+		//for(int j = 0; j< answer.size(); j++)
+		//{
+		//	System.out.printf("After %d: %s %s\n", j, answer.get(j).get(0), answer.get(j).get(1));
+		//}
+		return answer;
+		
+	}
 
 }
