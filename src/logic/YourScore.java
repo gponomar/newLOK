d38ee25e8dd206cb6082f048d68da9ba92c975a3
gponomar <gponomar@calpoly.edu>
2018-12-04 18:37:17 -0800

    fixed topscore smell

--- src/logic/YourScore.java
+++ src/logic/YourScore.java
@@ -111,13 +111,7 @@ public class YourScore {
 			}
 		});
     }
-	// action to be performed when back button is hit
-	private void homeBtnAction() {
-    	homepage.frm.remove(pnlHolderYourScore);
-    	homepage.frm.setContentPane(homepage.pnl);
-    	homepage.frm.validate();
-    	homepage.frm.repaint();
-	}
+	// action to be performed when done button is hit
 	private void doneBtnAction() throws IOException {
 		String myName = getname.getText();
 		getname.setText("Your Name");
