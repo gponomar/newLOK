e38214f150fcca61edabd3c7664070d51f65e609
Grant Parton <granteparton@gmail.com>
2018-12-04 20:13:10 -0800

    Ryan's two Junit tests

---
+++ src/test/RyansTests.java
@@ -0,0 +1,11 @@
+//Ryan Nevils Test Suite
+package test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestBaseLabel.class, TestJunitCounter.class})
+	public class RyansTests {
+	
+ }
